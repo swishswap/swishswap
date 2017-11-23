f45ae803a775f9f4e2a5515b2911d767f8794b29
IAMGROTH <marqus.eldar@gmail.com>
2017-11-23 14:56:07 +0100

    Konfiguration av gyrodata
    Skriptet GyroConfig innehåller övergången från gyroskopdata till quaternion i Unity!

---
+++ Assets/Scripts/GyroConfig.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+using System.Runtime.InteropServices;
+
+public class GyroCorrectionTest : MonoBehaviour {
+
+	private float alpha, beta, gamma;
+
+	[DllImport("__Internal")]
+	private static extern void SetupGyroscope();
+
+	// Use this for initialization
+	void Start () {
+
+
+		SetupGyroscope ();
+
+		alpha = 0;
+		beta = 0;
+		gamma = 0;
+	}
+	
+	public void setAlpha(int a){
+		alpha = a;
+	}
+
+	public void setBeta(int b){
+		beta = b;
+	}
+
+	public void setGamma(int g){
+		gamma = g;
+	}
+		
+	void FixedUpdate () {
+		//iPhone, standing
+		//gameObject.transform.SetPositionAndRotation(new Vector3(0, 0, 0), Quaternion.Euler(new Vector3(-beta, -gamma, alpha)));
+
+		//iPhone, horizontal
+		gameObject.transform.SetPositionAndRotation(new Vector3(0, 0, 0), Quaternion.Euler(new Vector3(gamma, -beta, alpha)));
+
+	}
+}
