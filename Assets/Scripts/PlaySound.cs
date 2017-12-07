3bff8815c38549c5ccd4c05b32a4b119868db887
jonatancoster <jonatan.coster@gmail.com>
2017-12-07 22:06:36 +0100

    Added sounds

---
+++ Assets/Scripts/PlaySound.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlaySound : MonoBehaviour {
+
+	public void playSound() {
+		this.GetComponent<AudioSource> ().Play ();
+	}
+}
